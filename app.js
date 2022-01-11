e8775e0a6442042020e34c715c083c590fff3468
HunterM7 <tonym7@yandex.ru>
2022-01-12 00:08:35 +0300

    upd

--- app.js
+++ app.js
@@ -7,8 +7,22 @@ document.getElementById('btnSubmit').onclick = () => {
 	const pricePerHour = 200;
 	const pricePerHourHoliday = 220;
 
-	let result = (hours * (pricePerHour * workDays + pricePerHourHoliday * holiDays) - penalties);
+	let result = convertNum((hours * (pricePerHour * workDays + pricePerHourHoliday * holiDays) - penalties));
 
 	document.getElementById('result').value = `${result}.00 руб.`;
 
+}
+
+const convertNum = function (x) {
+	let str = x.toString()
+	let length = Math.ceil(str.toString().length / 3);
+	let res = [];
+
+
+	for (let i = 0; i < length; i++) {
+		res.push(str.slice(-3));
+		str = str.slice(0, -3);
+	}
+
+	return res.reverse().join(' ')
 }
